5e6ceee38c550cfb982949a287b577df15f75122
Safa Nasirli <nasirli.safa1@gmail.com>
2021-09-10 22:39:46 -0400

    added Route

--- chef/src/App.js
+++ chef/src/App.js
@@ -1,9 +1,13 @@
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
+
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Route exact path="/" component={Home} />
+      <Route path="/recipe" />
     </div>
   );
 }
