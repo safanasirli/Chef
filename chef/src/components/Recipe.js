cae6cf39e0bf218ff3a95461d47a80cc1535fe41
Safa Nasirli <nasirli.safa1@gmail.com>
2021-09-12 00:16:57 -0400

    added styles to RecipeForm component

--- chef/src/components/Recipe.js
+++ chef/src/components/Recipe.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import styled from 'styled-components'
 import RecipeForm from './RecipeForm'
 import RecipeList from './RecipeList'
 function Recipe () {
   return (
     <div>
+    <H1>Search for Your Favorite Food. Find Thousands of Recipes, Including International Dishes.</H1>
       <RecipeForm />
       <RecipeList />
     </div>
@@ -11,3 +13,7 @@ function Recipe () {
 }
 
 export default Recipe
+const H1 =styled.h1`
+color: #002700;
+
+`
--- chef/src/components/RecipeForm.js
+++ chef/src/components/RecipeForm.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function RecipeForm () {
   return (
     <div>
       <form>
-        <input type='text' />
-        <button type='search'>Button</button>
+        <Input type='text' />
+        <Button type='submit'>Search</Button>
       </form>
     </div>
   )
 }
 
 export default RecipeForm
+
+const Input = styled.input`
+  border: 2px solid #002700;
+  border-radius: 8px;
+  height: 25px;
+  width: 50%;
+  margin-right: 1%;
+`
+const Button = styled.button`
+  background-color: #002700;
+  color: white;
+  font-weight: 700;
+  border: none;
+  border-radius: 8px;
+  width: 90px;
+  padding: 7px 10px 7px 10px;
+`
