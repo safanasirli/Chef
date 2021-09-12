814a71935063247277895a20bc3fef3eb3d681b2
Safa Nasirli <nasirli.safa1@gmail.com>
2021-09-12 01:32:30 -0400

    added styles to RecipeList component

--- chef/src/components/RecipeList.js
+++ chef/src/components/RecipeList.js
@@ -1,25 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function RecipeList ({ recipes }) {
   return (
-    <div>
+    <Card>
       {recipes.map((recipe, idx) => {
         return (
-          <card key='idx'>
-            <img src={recipe.recipe.image} alt={recipe.recipe.label} />
+          <CardItem key='idx'>
+            <Img src={recipe.recipe.image} alt={recipe.recipe.label} />
             <h1>{recipe.recipe.label}</h1>
             <h2>Calories:{recipe.recipe.calories}</h2>
-            <ul>
+            <button>Ingredients</button>
+            <ul id='ingredients'>
               {recipe.recipe.ingredients.map((ingredients, idx) => (
                 <li key='idx'>{ingredients.text}</li>
               ))}
             </ul>
             <a href={recipe.recipe.url}>See the recipe</a>
-          </card>
+          </CardItem>
         )
       })}
-    </div>
+    </Card>
   )
 }
 
 export default RecipeList
+const Card = styled.div`
+  display: flex;
+  flex-direction: row;
+  flew-wrap: wrap;
+`
+const CardItem = styled.div`
+  width: 400px;
+  height: 700px;
+  border: 1px solid black;
+  margin: 0 10px 0 10px;
+`
+const Img = styled.img`
+  width: 400px;
+  height: 400px;
+`
